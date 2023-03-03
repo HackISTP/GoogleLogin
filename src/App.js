b003e17f4bde8ad70ebe5e615dfae6896f900766
HackISTP <hyeminpak@gmail.com>
2023-03-03 22:10:25 +0900

    구글로그인 구현 테스트

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { auth } from "./firebase-config";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { useState } from "react";
 
 function App() {
+  const [userData, setUserData] = useState(null);
+  const handleGoogleLogin = () => {
+    const provider = new GoogleAuthProvider(); // provider 구글 설정
+    signInWithPopup(auth, provider) // 팝업창 띄워서 로그인
+      .then((data) => {
+        setUserData(data.user); // user data 설정
+        console.log(data); // console에 UserCredentialImpl 출력
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>구글 로그인 테스트</h3>
+      <button onClick={handleGoogleLogin}>로그인</button>
+      <h4>로그인하면 아래쪽에 이름이 나타납니다.</h4>
+      <div>
+        {userData
+          ? "당신의 이름은 : " + userData.displayName
+          : "로그인 버튼을 눌러주세요 :)"}
+      </div>
     </div>
   );
 }
